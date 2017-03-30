150684513e8f3c7f2deb2321f195811a238c7f5c
shumatanaka <shumat61411@gmail.com>
2017-03-30 12:04:16 +0000

    fifth commit

--- js/final-carousel.js
+++ js/final-carousel.js
@@ -9,3 +9,15 @@ $(document).ready(function(){
         }
     );
 });
+
+$('.animation').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('.animation').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
